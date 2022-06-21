dd650b85b481819123d1d901650f277807ec4cc0
Rafael Dias <rafaelarlindodias@gmail.com>
2022-06-20 22:55:18 -0300

    server js

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-// incluido o módulo http
 const http = require('http');
 
-// constantes host e porta
 const hostname = '127.0.0.1';
 const port = 3000;
 
-// método createServer, parâmetros outra função
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-type' : 'text/plain'}); // código de status HTTP
-  res.write(req.url); // solicitação do cliente. propriedade "url" contém parte da url
-  res.end("Servidor em funcionamento"); // escreve a resposta HTTP
+  res.writeHead(200, {'Content-type' : 'text/plain'});
+  res.write(req.url);
+  res.end("Servidor em funcionamento");
 });
 
 server.listen(port, hostname, () => {
